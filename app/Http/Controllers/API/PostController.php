865d9a5d8ad8b334ca1d98dec3a93ea878998d53
dusainbolt <dulh181199@gmail.com>
2020-10-05 21:55:16 +0700

    fix new feed

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -12,17 +12,20 @@ use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
-
-
-    public function myNewFeed(Request $request){
-        $limit = $request->limit ? $request->limit : 5;
-//        $limitComment = $request->limitComment ? $request->limitComment : 10;
-        $listPost  = Posts::orderBy('created_at', 'desc')->take($limit)->get();
-        foreach ($listPost as $post){
+    public function myNewFeed(Request $request)
+    {
+        $limit = $request->limit ? $request->limit : 10;
+        $typeFilter = $request->type;
+        $type = $typeFilter && $typeFilter != POST_TYPE_HOST ? $typeFilter : FILTER_ALL;
+        $isText = $request->type == 5 ? BOOL_TRUE : BOOL_FALSE;
+        $listPost = Posts::where('type', $isText || $type != FILTER_ALL ? "=" : ">", $isText ? 0 : $type)
+                         ->orderBy('created_at', 'desc')->take($limit)->get();
+        foreach ($listPost as $post) {
             $data[] = getResponseNewFeed($post);
         }
         return response()->json(\getResponse($data, META_CODE_SUCCESS));
     }
+
     /**
      * Display a listing of the resource.
      *
@@ -37,49 +40,51 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
         $validate = getValidate(VALIDATE_POST);
-        $validator = Validator::make($request->all(), $validate[0],$validate[1]);
-        if($validator->fails()){
+        $validator = Validator::make($request->all(), $validate[0], $validate[1]);
+        if ($validator->fails()) {
             return response()->json(\getResponse([], META_CODE_ERROR, $validator->errors()->first()), Response::HTTP_BAD_REQUEST);
-        }else if($request->type == 1 && !isset($request->images)){
+        } else if ($request->type == 1 && !isset($request->images)) {
             return response()->json(\getResponse([], META_CODE_ERROR, IMAGES_REQUIRED), Response::HTTP_BAD_REQUEST);
         }
         $arrKey = ["type", "tag", "subjectId", "caption"];
-        $newPost = mapDataModel($arrKey, new Posts(),$request, USER_ID, Auth::user()->id);
+        $newPost = mapDataModel($arrKey, new Posts(), $request, USER_ID, Auth::user()->id);
         $newPost->content = $this->genderContentPost($request);
         $newPost->save();
         return response()->json(\getResponse(getResponseNewFeed($newPost), META_CODE_SUCCESS, POST_NEW_SUCCESS));
     }
 
-    protected function genderContentPost($request){
-        switch ($request->type){
+    protected function genderContentPost($request)
+    {
+        switch ($request->type) {
             case POST_TYPE_STATUS:
                 return $request->payload;
             case POST_TYPE_IMAGE:
                 $content = "";
                 $listImage = $request->images;
-                foreach($listImage as $item){
-                    if($item->getSize() > 1024 * 1024){
+                foreach ($listImage as $item) {
+                    if ($item->getSize() > 1024 * 1024) {
                         continue;
                     }
-                    $fullpath = UPLOAD_DIR .''.randomString(20).''.strtotime(date(FORMAT_CURRENT_TIME));
+                    $fullpath = UPLOAD_DIR . '' . randomString(20) . '' . strtotime(date(FORMAT_CURRENT_TIME));
                     Storage::disk('s3')->put($fullpath, file_get_contents($item), 'public');
-                    $content  .= ",".Storage::disk('s3')->url($fullpath);
+                    $content .= "," . Storage::disk('s3')->url($fullpath);
                 }
-                return  ltrim($content, ",");
-            default: return null;
+                return ltrim($content, ",");
+            default:
+                return null;
         }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -91,8 +96,8 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -103,7 +108,7 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
