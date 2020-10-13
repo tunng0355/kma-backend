7af3094228911c4e987a15309d6fe514256e10c7
dusainbolt <dulh181199@gmail.com>
2020-10-13 22:15:13 +0700

    fix list fr no active

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,12 +8,16 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function getListFriends(Request $request){
-        $limit           = $request->limit ? $request->limit : LIMIT_LIST_FRIENDS;
-        $offset          = $request->offset ? $request->offset : 0;
-        $searchText      = $request->searchText ? $request->searchText : "%";
-        $listFriends     = UserInfo::where('fullName', 'like', '%'.$searchText.'%')
-                         ->offset($offset)->limit($limit)->orderBy('fullName')->get();
+    public function getListFriends(Request $request)
+    {
+        $limit = $request->limit ? $request->limit : LIMIT_LIST_FRIENDS;
+        $offset = $request->offset ? $request->offset : 0;
+        $searchText = $request->searchText ? $request->searchText : "%";
+        $listFriends = UserInfo::where('fullName', 'like', '%' . $searchText . '%')
+                      ->join('users', function ($join) {
+                            $join->on('user_info.userId', '=', 'users.id')
+                            ->where('status', '=', 1);})
+                      ->offset($offset)->limit($limit)->orderBy('fullName')->get();
         return response()->json(\getResponse(getResponseListFriends($listFriends, Auth::user()->id), META_CODE_SUCCESS, SEND_EMAIL_SUCCESS));
     }
 }
